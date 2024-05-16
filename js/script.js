a11248cb982150af220a82bd443282e78d6786a9
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-05-16 17:40:03 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked() {
+
+  // input
+
+  let day = document.getElementById('day')
+  let age  = document.getElementById('age').value
+
+  // process
+  // output
+
+  if ((day == "tuesday" || day == "thursday")|| (age > 12 && age < 21 )) {
+    document.getElementById("user-info").innerHTML = "Your pricing is: student pricing"
+  } else {
+    document.getElementById("user-info").innerHTML = "Your pricing is: regular pricing"
+  }
+}
